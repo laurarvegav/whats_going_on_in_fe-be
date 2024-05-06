e342004404ffd3073f3d466688c2422177c45d06
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-06 15:57:33 -0600

    Comoplete fe part

--- fe/src/App.js
+++ fe/src/App.js
@@ -3,16 +3,24 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [welcomeMessage, setWelcomeMessage] = useState(null);
+  const [messages, setMessages] = useState([])
 
   useEffect(() => {
     fetchMessage();
   }, []);
+
+  useEffect(() => {
+    if (welcomeMessage) {
+    showMessages()
+    }
+  }, [welcomeMessage])
 
   function fetchMessage() {
     fetch('http://localhost:3001/api/v1/welcome')
       .then(response => {
         return response.json();
       }).then(json => {
+        console.log(json.data.attributes.body);
         setWelcomeMessage(json.data.attributes.body);
       }).catch(error => {
         console.log(error);
@@ -20,8 +28,18 @@ function App() {
       })
   }
 
+
+  const showMessages = () => {
+    const newMessages = welcomeMessage.map(message => {
+      return (
+        <div>{message.text}</div>
+      )
+    })
+    setMessages(newMessages)
+  }
+
   return (
-    <h1>{welcomeMessage}</h1>
+    <h1>{messages}</h1>
   );
 }
 
